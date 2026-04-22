b3ef3b2b5e5dec99f6913545975ceb756d802dc5
EduardoBougo <eduardobougo@gmail.com>
2026-04-22 13:46:15 -0300

    a

--- 001-flappy-bird/scenes/pipe.gd
+++ 001-flappy-bird/scenes/pipe.gd
@@ -13,3 +13,7 @@ func _process(delta):
 func _on_body_entered(_body):
 	# Quando qualquer corpo entrar na area, emite o sinal "hit"
 	hit.emit() 
+
+
+func _on_hit() -> void:
+	pass # Replace with function body.
